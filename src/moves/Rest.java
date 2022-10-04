cdc6e0843fdffd63d16062aceaad806e1322f633
DimaTivator <andriianov164@gmail.com>
2022-10-04 23:57:17 +0300

    Rest first commit

--- src/moves/Rest.java
+++ src/moves/Rest.java
@@ -1,11 +1,22 @@
 package moves;
 
-import ru.ifmo.se.pokemon.StatusMove;
+import ru.ifmo.se.pokemon.*;
 
 public class Rest extends StatusMove {
 
-    public Rest(String name, int level) {
-        super(name, level);
-        System.out.println("LOLOLOL");
+    public Rest() {
+        super(Type.PSYCHIC, 0, 0);
+    }
+
+    @Override
+    protected void applySelfEffects(Pokemon attacker) {
+        Effect sleep = new Effect().condition(Status.SLEEP).turns(2);
+        attacker.addEffect(sleep);
+        attacker.restore();
+    }
+
+    @Override
+    protected String describe() {
+        return "использует Rest";
     }
 }
