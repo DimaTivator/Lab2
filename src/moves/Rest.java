fd39f8dbfbec0fed69a4bde46ffc5c74f509fb57
DimaTivator <andriianov164@gmail.com>
2022-10-04 23:48:04 +0300

    first commit

---
+++ src/moves/Rest.java
@@ -0,0 +1,11 @@
+package moves;
+
+import ru.ifmo.se.pokemon.StatusMove;
+
+public class Rest extends StatusMove {
+
+    public Rest(String name, int level) {
+        super(name, level);
+        System.out.println("LOLOLOL");
+    }
+}
